2bff9706cdaf7f2069d630ad8180bee260f9e1a6
Frewen.Wong <frewen1225@gmail.com>
2020-07-03 08:48:15 +0800

    [ADD] add animation demo

---
+++ nyx-android-app/src/main/java/com/frewen/android/demo/samples/animation/ui/main/PropertyAnimationFragment.java
@@ -0,0 +1,56 @@
+package com.frewen.android.demo.samples.animation.ui.main;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.frewen.android.demo.R;
+import com.frewen.aura.framework.fragment.BaseFragment;
+
+import androidx.annotation.Nullable;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class PropertyAnimationFragment extends BaseFragment {
+
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    private PageViewModel pageViewModel;
+
+    public static PropertyAnimationFragment newInstance(int index) {
+        PropertyAnimationFragment fragment = new PropertyAnimationFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt(ARG_SECTION_NUMBER, index);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        pageViewModel = ViewModelProviders.of(this).get(PageViewModel.class);
+        int index = 1;
+        if (getArguments() != null) {
+            index = getArguments().getInt(ARG_SECTION_NUMBER);
+        }
+        pageViewModel.setIndex(index);
+    }
+
+    @Override
+    protected View createView(LayoutInflater inflater, ViewGroup container, boolean attachToRoot) {
+        View root = inflater.inflate(R.layout.fragment_animation_property, container, false);
+        final TextView textView = root.findViewById(R.id.section_label);
+        pageViewModel.getText().observe(getActivity(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}

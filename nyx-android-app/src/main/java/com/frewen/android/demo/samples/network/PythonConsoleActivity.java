8325474249437bc10d7d0d2897893095ad371ea3
Frewen.Wong <frewen1225@gmail.com>
2020-07-09 10:42:11 +0800

    [ADD] add python demo

---
+++ nyx-android-app/src/main/java/com/frewen/android/demo/samples/network/PythonConsoleActivity.java
@@ -0,0 +1,18 @@
+package com.frewen.android.demo.samples.network;
+
+import android.view.ViewTreeObserver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @filename: PythonConsoleActivity
+ * @introduction:
+ * @author: Frewen.Wong
+ * @time: 2020/6/28 21:14
+ *         Copyright ©2020 Frewen.Wong. All Rights Reserved.
+ */
+public abstract class PythonConsoleActivity extends AppCompatActivity
+        implements ViewTreeObserver.OnGlobalLayoutListener, ViewTreeObserver.OnScrollChangedListener {
+
+
+}

d8e09e09d437befe6e4b42dfd3faeb0ac5c6bfa0
Frewen.Wong <frewen1225@gmail.com>
2020-09-07 13:31:18 +0800

    [ADD] add fragment tiktok

--- nyx-android-app/src/main/java/com/frewen/android/demo/samples/tiktok/home/MainFragment.kt
+++ nyx-android-app/src/main/java/com/frewen/android/demo/samples/tiktok/home/MainFragment.kt
@@ -1,16 +1,12 @@
 package com.frewen.android.demo.samples.tiktok.home
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
-import androidx.lifecycle.observe
 import com.frewen.android.demo.R
-import com.frewen.android.demo.samples.tiktok.fragments.MineViewModel
 import com.frewen.aura.framework.fragment.BaseButterKnifeFragment
-import com.frewen.aura.framework.fragment.BaseFragment
+import java.util.*
 
 /**
  * @filename: HomeFragment
@@ -22,17 +18,30 @@ import com.frewen.aura.framework.fragment.BaseFragment
  */
 class MainFragment : BaseButterKnifeFragment() {
 
+    private val fragments = ArrayList<Fragment>()
     private lateinit var mineViewModel: MainViewModel
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         mineViewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+
+        initView()
+    }
+
+    /**
+     * 初始化View
+     */
+    private fun initView() {
     }
 
     override fun getLayoutId(): Int {
         return R.layout.fragment_tiktok_home_video_feed
     }
+
+
+
+
 
 
 }

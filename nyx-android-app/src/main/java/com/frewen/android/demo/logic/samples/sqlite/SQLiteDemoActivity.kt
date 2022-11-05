7ce955138125275b38c5f33ff512a135d67be4ea
Frewen.Wong <frewen1225@gmail.com>
2022-11-05 14:39:53 +0800

    [modifdy] move navigation

--- nyx-android-app/src/main/java/com/frewen/android/demo/logic/samples/sqlite/SQLiteDemoActivity.kt
+++ nyx-android-app/src/main/java/com/frewen/android/demo/logic/samples/sqlite/SQLiteDemoActivity.kt
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-import com.frewen.android.aura.annotations.ActivityDestination
 import com.frewen.android.demo.R
 
 class SQLiteDemoActivity : AppCompatActivity() {
